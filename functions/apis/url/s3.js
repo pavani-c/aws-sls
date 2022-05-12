b9cb0a9f3a48750d1171d9c274a411b79adac557
pavani-c <pawani.chilumula@gmail.com>
2022-05-12 14:08:02 -0700

    csv parser and lambda layer code

--- functions/apis/url/s3.js
+++ functions/apis/url/s3.js
@@ -1,24 +1,67 @@
 'use strict';
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const csv = require('@fast-csv');
 
 async function uploadUrl(event, context, cb) {
-    logger.info("Event: ", JSON.stringify(event, "", 2));
-    logger.info("Context: ", JSON.stringify(context, "", 2));
-    let type;
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    console.log("Request received");
+
+   /* // Extract file content
+    let fileContent = event.isBase64Encoded ? Buffer.from(event.body, 'base64') : event.body;
+
+    // Generate file name from current timestamp
+    let fileName = `${Date.now()}`;
+
+    // Determine file extension
+    let contentType = event.headers['content-type'] || event.headers['Content-Type'];
+    let extension = 'csv';//contentType ? mime.extension(contentType) : '';
+
+    let fullFileName = extension ? `${fileName}.${extension}` : fileName;
+
+    // Upload the file to S3
+    try {
+        let data = await s3.putObject({
+            Bucket: "ee-taxcode-files",
+            Key: fullFileName,
+            Body: fileContent,
+            Metadata: {}
+        }).promise();
+
+        console.log("Successfully uploaded file", fullFileName);
+        return "Successfully uploaded";
+
+    } catch (err) {
+        console.log("Failed to upload file", fullFileName, err);
+        throw err;
+    }*/
 };
+
 async function getUrl(event, context, cb) {
-    logger.info("Event: ", JSON.stringify(event, "", 2));
-    logger.info("Context: ", JSON.stringify(context, "", 2));
-    let url;
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+
+       // Get the file from the bucket
+        let key = 'SAMPLE TAX LOCATOR FILE.csv';
+
+        const params = {
+            Bucket: 'ee-taxcode-files',
+            Key: key,
+        };
+
+        const csvFile = s3.getObject(params).createReadStream();
+
+        let parserFcn = new Promise((resolve, reject) => {
+            const parser = csv
+                .parseStream(csvFile, {headers: true})
+                .on("data", function (data) {
+                    console.log('Data parsed: ', data);
+                })
+                .on("end", function () {
+                    resolve("csv parse process finished");
+                })
+                .on("error", function () {
+                    reject("csv parse process failed");
+                });
+        });
+
 };
 
 module.exports = {
